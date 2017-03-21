7b53dbc729cc7c453be05d050c5fe02d3a55ab29
Okan Çetin <okancetin44@gmail.com>
2017-03-21 03:55:55 +0300

    color palette wip

--- src/app/ColorPicker.js
+++ src/app/ColorPicker.js
@@ -2,8 +2,25 @@ import React from 'react'
 import reactCSS from 'reactcss'
 import { SketchPicker } from 'react-color'
 import ColorHelper from 'react-color/lib/helpers/color'
+import * as Colors from 'material-ui/styles/colors'
 
 export class ColorPicker extends React.Component {
+
+    static palette = Object.keys(Colors).reduce((result, key) => {
+        var test = /([^A\d]+)([A?\d]+)?/.exec(key);
+        var base = test[1];
+        var tone = test[2];
+
+        if (base && !tone) {
+            tone = base;
+            base = "";
+        }
+
+        result[base] = result[base] || {};
+        result[base][tone] = Colors[key]
+
+        return result;
+    }, {});
 
     constructor(props) {
         super(props);
@@ -73,10 +90,43 @@ export class ColorPicker extends React.Component {
                 <div style={styles.swatch} onClick={this.handleClick}>
                     <div style={styles.color} />
                 </div>
-                {this.state.displayColorPicker ? <div style={styles.popover}>
-                    <div style={styles.cover} onClick={this.handleClose} />
-                    <SketchPicker color={this.state.color.rgb} onChange={this.handleChange} />
-                </div> : null}
+                {this.state.displayColorPicker ?
+                    <table style={{ position: 'absolute', backgroundColor: "#eee" }}>
+                        <tr>
+                            <td>
+                                <table style={{ borderCollapse: 'collapse' }}>
+                                    {Object.keys(ColorPicker.palette).map(base =>
+                                        <tr key={base}>
+                                            <td style={{
+                                                backgroundColor: ColorPicker.palette[base]["500"],
+                                                width: 20,
+                                                height: 20
+                                            }}></td>
+                                        </tr>)
+                                    }
+                                </table>
+                            </td>
+                            <td>
+                                <table style={{ borderCollapse: 'collapse' }}>
+                                    {Object.keys(ColorPicker.palette["red"]).map(color =>
+                                        <tr key={color}>
+                                            <td style={{
+                                                backgroundColor: ColorPicker.palette["red"][color],
+                                                width: 100,
+                                                height: 20
+                                            }}>{color}</td>
+                                        </tr>)
+                                    }
+                                </table>
+                            </td>
+                        </tr>
+                    </table>
+
+                    /*<div style={styles.popover}>
+                        <div style={styles.cover} onClick={this.handleClose} />
+                        <SketchPicker color={this.state.color.rgb} onChange={this.handleChange} />
+                    </div>*/
+                    : null}
 
             </div>
         )
